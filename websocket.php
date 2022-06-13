ed53105cdca412737586039fa9c0e547562e2a17
Teroaz <neoxdelta@gmail.com>
2022-06-13 10:14:41 +0200

    websocket

--- websocket.php
+++ websocket.php
@@ -8,6 +8,8 @@ class WebSocket
 
     private $clients = array();
 
+    private $globalDataJson = "";
+
 
     /**
      * @throws Exception
@@ -27,6 +29,10 @@ class WebSocket
         socket_listen($socket);
 
         $this->clients = array();
+
+        $dataFile = new DataFile();
+        $this->globalDataJson = $dataFile->getData();
+
         $this->process();
     }
 
@@ -39,27 +45,12 @@ class WebSocket
             if ($client === false) {
                 throw new Exception('socket_accept() failed: ' . socket_strerror(socket_last_error()));
             }
+
             $this->clients[] = $client;
+
+            foreach ($this->clients as $cl) {
+                socket_write($cl, $this->globalDataJson);
+            }
         }
     }
 }
-/*
-$request = socket_read($client, 5000);
-preg_match('#Sec-WebSocket-Key: (.*)\r\n#', $request, $matches);
-$key = base64_encode(pack(
-    'H*',
-    sha1($matches[1] . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')
-));
-$headers = "HTTP/1.1 101 Switching Protocols\r\n";
-$headers .= "Upgrade: websocket\r\n";
-$headers .= "Connection: Upgrade\r\n";
-$headers .= "Sec-WebSocket-Version: 13\r\n";
-$headers .= "Sec-WebSocket-Accept: $key\r\n\r\n";
-socket_write($client, $headers, strlen($headers));
-
-while (true) {
-    sleep(1);
-    $content = 'Now: ' . time();
-    $response = chr(129) . chr(strlen($content)) . $content;
-    socket_write($client, $response);
-}*/
